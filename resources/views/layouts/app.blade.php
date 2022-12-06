bf5cf7a577ced8355d87d19059fbf0692cdda370
Wasim Hyder <wasim@creativefaze.com>
2022-12-06 20:36:47 +0500

    Added asset compilaton

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,6 @@
+@include('layouts.header')
+<div class="content">
+  @yield('content')
+</div>
+@include('layouts.footer')
+  
---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,11 @@
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+  <link rel="icon" type="image/x-icon" href="{{ asset('images/favicon.png') }}">
+  <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+  <title>IMDb Dashboard</title>
+</head>
+<body>
